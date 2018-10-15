c969e7cfd82014f9dd0397f4bf285caf573ab9a0
Reeves Westover <krwestover@gmail.com>
2018-10-15 17:46:04 -0600

    Added all Dictionary code

---
+++ DataStructures_2-7/Controllers/DictionaryController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataStructures_2_7.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        public static Dictionary<string, int> oDictionary = new Dictionary<string, int>();
+        
+        
+        // GET: Dictionary
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Add one item to Dictionary
+        public ActionResult AddOne()
+        {
+            oDictionary.Add(("New Entry #" +(oDictionary.Count+1)),(oDictionary.Count+1));
+            return View("Index");
+        }
+
+        // Add huge list to Dictionary
+        public ActionResult AddHugeList()
+        {
+            oDictionary.Clear();
+
+            for (int i = 1; i <= 2000; i++)
+            {
+                oDictionary.Add(("New Entry #" + i), i);
+            }
+            return View("Index");
+        }
+
+        // Displays the Dictionary
+        public ActionResult DisplayDictionary()
+        {
+            foreach (KeyValuePair<string,int> item in oDictionary)
+            {
+                ViewBag.Dictionary += "<li>New Entry #" + item.Value.ToString() + "</li>";
+            }
+
+            return View("Index");
+        }
+
+        // Deletes item from Dictionary
+        public ActionResult DeleteFromDictionary()
+        {
+            oDictionary.Remove("New Entry #"+oDictionary.Count);
+            return View("Index");
+        }
+
+        // Clears Dictionary
+        public ActionResult ClearDictionary()
+        {
+            oDictionary.Clear();
+            return View("Index");
+        }
+
+        // Searches for item in Dictionary
+        public ActionResult SearchDictionary()
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+
+            if (oDictionary.ContainsKey("New Entry #7"))
+            {
+                ViewBag.SearchResult = "Found";
+            }
+            else
+            {
+                ViewBag.SearchResult = "Not Found";
+            }
+
+            sw.Stop();
+
+            TimeSpan ts = sw.Elapsed;
+
+            ViewBag.StopWatch = ts.Milliseconds + "ms";
+
+            return View("Index");
+        }
+    }
+}
